175c337adab8d66547e810ecc053157668a89233
pawkedzia <pawel@radlab.dev>
2025-12-24 00:39:56 +0100

    Enable LangChain RAG plugin in the main utilities registry.

--- llm_router_plugins/utils/registry.py
+++ llm_router_plugins/utils/registry.py
@@ -5,8 +5,7 @@ Central registry for utils plugins.
 from llm_router_plugins.utils.rag.langchain_plugin import LangchainRAGPlugin
 
 MAIN_UTILS_REGISTRY = {
-    # LangchainRAGPlugin is available as a module, not as a service
-    LangchainRAGPlugin.name: None,
+    LangchainRAGPlugin.name: LangchainRAGPlugin,
 }
 
 UTILS_HOSTS_DEFINITION = {}
