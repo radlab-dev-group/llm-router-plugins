69743233a5f32dbec4c425746a5e0ac6d627ac46
pawkedzia <pawel@radlab.dev>
2026-07-10 22:16:16 +0200

    **refactor:** remove unnecessary `# noqa: F401` comments and reformat imports for PEP 8 compliance in `fast_masker.rules` module

--- llm_router_plugins/maskers/fast_masker/rules/__init__.py
+++ llm_router_plugins/maskers/fast_masker/rules/__init__.py
@@ -4,100 +4,100 @@
 Package that contains concrete masking rule implementations.
 """
 
-from llm_router_plugins.maskers.fast_masker.rules.credit_card_rule import (  # noqa: F401
+from llm_router_plugins.maskers.fast_masker.rules.credit_card_rule import (
     CreditCardRule,
 )
 from llm_router_plugins.maskers.fast_masker.rules.nrb_rule import (
     NrbRule,
-)  # noqa: F401
+)
 from llm_router_plugins.maskers.fast_masker.rules.vin_rule import (
     VinRule,
-)  # noqa: F401
-from llm_router_plugins.maskers.fast_masker.rules.mac_address_rule import (  # noqa: F401
+)
+from llm_router_plugins.maskers.fast_masker.rules.mac_address_rule import (
     MacAddressRule,
 )
 from llm_router_plugins.maskers.fast_masker.rules.jwt_rule import (
     JwtRule,
-)  # noqa: F401
+)
 from llm_router_plugins.maskers.fast_masker.rules.sim_card_rule import (
     SimCardRule,
-)  # noqa: F401
+)
 from llm_router_plugins.maskers.fast_masker.rules.ssl_cert_rule import (
     SslCertRule,
-)  # noqa: F401
-from llm_router_plugins.maskers.fast_masker.rules.transaction_ref_rule import (  # noqa: F401
+)
+from llm_router_plugins.maskers.fast_masker.rules.transaction_ref_rule import (
     TransactionRefRule,
 )
-from llm_router_plugins.maskers.fast_masker.rules.invoice_number_rule import (  # noqa: F401
+from llm_router_plugins.maskers.fast_masker.rules.invoice_number_rule import (
     InvoiceNumberRule,
 )
-from llm_router_plugins.maskers.fast_masker.rules.order_number_rule import (  # noqa: F401
+from llm_router_plugins.maskers.fast_masker.rules.order_number_rule import (
     OrderNumberRule,
 )
 from llm_router_plugins.maskers.fast_masker.rules.car_plate_rule import (
     CarPlateRule,
-)  # noqa: F401
+)
 from llm_router_plugins.maskers.fast_masker.rules.health_id_rule import (
     HealthIdRule,
-)  # noqa: F401
+)
 from llm_router_plugins.maskers.fast_masker.rules.id_card_rule import (
     IdCardRule,
-)  # noqa: F401
+)
 from llm_router_plugins.maskers.fast_masker.rules.passport_rule import (
     PassportRule,
-)  # noqa: F401
-from llm_router_plugins.maskers.fast_masker.rules.pesel_tagged_rule import (  # noqa: F401
+)
+from llm_router_plugins.maskers.fast_masker.rules.pesel_tagged_rule import (
     PeselTaggedRule,
 )
 from llm_router_plugins.maskers.fast_masker.rules.pesel_rule import (
     PeselRule,
-)  # noqa: F401
-from llm_router_plugins.maskers.fast_masker.rules.phone_international_rule import (  # noqa: F401
+)
+from llm_router_plugins.maskers.fast_masker.rules.phone_international_rule import (
     PhoneInternationalRule,
 )
 from llm_router_plugins.maskers.fast_masker.rules.phone_rule import (
     PhoneRule,
-)  # noqa: F401
+)
 from llm_router_plugins.maskers.fast_masker.rules.email_rule import (
     EmailRule,
-)  # noqa: F401
+)
 from llm_router_plugins.maskers.fast_masker.rules.url_rule import (
     UrlRule,
-)  # noqa: F401
-from llm_router_plugins.maskers.fast_masker.rules.ip_rule import IpRule  # noqa: F401
-from llm_router_plugins.maskers.fast_masker.rules.bank_account_rule import (  # noqa: F401
+)
+from llm_router_plugins.maskers.fast_masker.rules.ip_rule import IpRule
+from llm_router_plugins.maskers.fast_masker.rules.bank_account_rule import (
     BankAccountRule,
 )
-from llm_router_plugins.maskers.fast_masker.rules.date_number_rule import (  # noqa: F401
+from llm_router_plugins.maskers.fast_masker.rules.date_number_rule import (
     DateNumberRule,
 )
 from llm_router_plugins.maskers.fast_masker.rules.date_word_rule import (
     DateWordRule,
-)  # noqa: F401
+)
 from llm_router_plugins.maskers.fast_masker.rules.krs_rule import (
     KrsRule,
-)  # noqa: F401
+)
 from llm_router_plugins.maskers.fast_masker.rules.money_rule import (
     MoneyRule,
-)  # noqa: F401
+)
 from llm_router_plugins.maskers.fast_masker.rules.nip_rule import (
     NipRule,
-)  # noqa: F401
-from llm_router_plugins.maskers.fast_masker.rules.postal_code_rule import (  # noqa: F401
+)
+from llm_router_plugins.maskers.fast_masker.rules.postal_code_rule import (
     PostalCodeRule,
 )
 from llm_router_plugins.maskers.fast_masker.rules.regon_rule import (
     RegonRule,
-)  # noqa: F401
+)
 from llm_router_plugins.maskers.fast_masker.rules.social_id_rule import (
     SocialIdRule,
-)  # noqa: F401
+)
 from llm_router_plugins.maskers.fast_masker.rules.time_rule import (
     TimeRule,
-)  # noqa: F401
+)
 from llm_router_plugins.maskers.fast_masker.rules.ssn_rule import (
     SsnRule,
-)  # noqa: F401
+)
 from llm_router_plugins.maskers.fast_masker.rules.eu_vat_rule import (
     EuVatRule,
-)  # noqa: F401
+)
